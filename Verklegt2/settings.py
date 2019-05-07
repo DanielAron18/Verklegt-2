be6055200fb778364a6d2ccd93e70fa69f761fa6
Daniel18 <daniel.aron1111@gmail.com>
2019-05-07 10:46:11 +0000

    fixed so server runs

--- Verklegt2/settings.py
+++ Verklegt2/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'castle_apartments.apps.CastleApartmentsConfig',
-    'manufacturers.apps.ManufacturersConfig'
 ]
 
 MIDDLEWARE = [
